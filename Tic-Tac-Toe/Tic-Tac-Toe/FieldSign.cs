80ac8288ebfcae607fa5b20828de28e78b39f265
Janina Kruppke <janina-kruppke@t-online.de>
2023-09-01 23:04:25 +0200

    Added first logic
    Toggles between player 1 and 2. You are able to mark the cells with your symbole.

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/FieldSign.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+    /// <summary>
+    /// Current value of a cell in the game
+    /// </summary>
+    public enum FieldSign
+    {
+        /// <summary>
+        /// Cell hasn't been clicked yet
+        /// </summary>
+        Free,
+        /// <summary>
+        /// Cell is marked with a cross
+        /// </summary>
+        Cross,
+        /// <summary>
+        /// Cell is marked with a circle
+        /// </summary>
+        Circle        
+    }
+}
--- Tic-Tac-Toe/Tic-Tac-Toe/MainWindow.xaml.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Tic_Tac_Toe
 {
@@ -21,6 +11,20 @@ namespace Tic_Tac_Toe
     public partial class MainWindow : Window
     {
         #region Variables
+        /// <summary>
+        /// Contains current state of every cell during the active game
+        /// </summary>
+        private FieldSign[] currentGameState;
+
+        /// <summary>
+        /// True if it's player1's turn (x)
+        /// </summary>
+        private bool player1Turn;
+
+        /// <summary>
+        /// True if the game has ended
+        /// </summary>
+        private bool gameEnded;
 
         #endregion
 
@@ -36,9 +40,73 @@ namespace Tic_Tac_Toe
         }
         #endregion
 
+        #region Methods
+        /// <summary>
+        /// Starts a new game and resets all values to default
+        /// </summary>
         void StartNewGame()
         {
+            // Create a new blanc array of free cells
+            currentGameState = new FieldSign[9];
+            for(int i = 0; i < currentGameState.Length; i++)
+            {
+                currentGameState[i] = FieldSign.Free;
+            }
 
+            // Player1 starts the game
+            player1Turn = true;
+
+            // Iterate every cell (button) of the grid
+            playground.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                // Clear every cell and set default design of field
+                button.Content = string.Empty;
+                button.Background = Brushes.White;
+                button.Foreground = Brushes.Red;
+            });
+
+            // Make sure game starts and isn't finished yet
+            gameEnded = false;
         }
+
+        /// <summary>
+        /// Handles a button (cell) click event
+        /// </summary>
+        /// <param name="sender"> The clicked button (cell) </param>
+        /// <param name="e"> The events of the click </param>
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            // Start a new game on the click after it finished
+            if (gameEnded)
+            {
+                StartNewGame();
+                return;
+            }
+
+            // Cast sender to a button
+            Button button = (Button)sender;
+
+            // Find the button's position in field 
+            int column = Grid.GetColumn(button);
+            int row = Grid.GetRow(button);
+
+            int index = column + (row * 3);
+
+            // Don't do anything if the cell has already a sign in it
+            if(currentGameState[index] != FieldSign.Free)
+            {
+                return;
+            }
+
+            // Set the cell's sign based on which players turn it is
+            currentGameState[index] = player1Turn? FieldSign.Cross : FieldSign.Circle;
+
+            // Set button text
+            button.Content = player1Turn ? "X" : "O";
+
+            // Toggle the players turns
+            player1Turn ^= true;
+        }
+        #endregion
     }
 }
